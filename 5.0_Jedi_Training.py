6b20beec5ed62f2c9f69af4403468e365cf615d1
buiste <buiste@urbandaleschools.com>
2019-10-18 13:46:59 -0500

    commit

--- 5.0_Jedi_Training.py
+++ 5.0_Jedi_Training.py
@@ -3,20 +3,22 @@
 '''
  1. Make the following program work.
    '''  
-     print("This program takes three numbers and returns the sum.")
-     total = 0
+print("This program takes three numbers and returns the sum.")
+total = 0
 
-     for i in range(3):
-         x = input("Enter a number: ")
-         total = total + i
-     print("The total is:", x)
-  
+for i in range(3):
+    x = int(input("Enter 3 numbers: "))
+    total = total + x
+print("The total is:",total)
+
 
 
 '''
   2. Write a Python program that will use a FOR loop to print the even
      numbers from 2 to 100, inclusive.
 '''
+for i in range(2,101,2):
+    print(i)
 
 
 
@@ -27,7 +29,19 @@
      10 down to, and including, 0. Then print the words Blast off! Remember, use
      a WHILE loop, don't use a FOR loop.
 '''
+i=11
+while i >0:
+    i-=1
+    print(i)
+print("Blast off!")
 
+
+
+
+
+
+# done = False
+# while not done:
 
 
 
@@ -36,7 +50,9 @@
 '''
   4. Write a program that prints a random integer from 1 to 10 (inclusive).
 '''
-
+import random
+i= random.randrange(1,11)
+print(i)
 
 
 
@@ -52,3 +68,8 @@
      if statements.
       
 '''
+total=0
+for i in range(7):
+   num=int(input("Please give me a number!"))
+   total+=num
+print("Your total comes to:",total)
--- 5.1_Coin_Toss.py
+++ 5.1_Coin_Toss.py
@@ -6,6 +6,22 @@ COIN TOSS PROGRAM
 3.) Add a loop so that the program does this 50 times.
 4.) Create a running total for the number of heads and the number of tails and print the total at the end.
 '''
+import random
+o=0
+x=0
+for i in range(50):
+    xo= random.randrange(0, 2)
+    if xo == 0:
+        o+=1
+        print("heads")
+    else:
+        x+=1
+        print("tails")
+print("number of tails",x)
+print("number of heads",o)
+
+
+
 
 
 
