ff0d74225b67d132c73e0eadf122b91ba63f7aed
buiste <buiste@urbandaleschools.com>
2019-10-24 14:14:00 -0500

    commit

--- 5.2_Roshambo.py
+++ 5.2_Roshambo.py
@@ -10,6 +10,59 @@ Add conditional statements to figure out who wins and keep the records
 When the user quits print a win/loss record
 
 '''
+com=0
+tie=0
+user=0
+done= False
+while not done:
+    spr= int(input("1.Rock 2.paper 3.scissors... Click 0 to quit"))
+    import random
+    num=random.randrange(1,4)
+    if num == 1:
+        rps= "Rock"
+    elif num ==2 :
+        rps= "Paper"
+    elif num == 3:
+        rps= "Scissors"
+    if spr==1 and rps=="Paper":
+        print("Paper you lose")
+        com+=1
+    elif spr== 2 and rps=="Scissors":
+        print("Scissors you lose")
+        com+=1
+    elif spr==3 and rps=="Rock":
+        print("Rock you lose")
+        com+=1
+    if spr==1 and rps=="Scissors":
+        print("Scissors you win")
+        user+=1
+    elif spr== 2 and rps=="Rock":
+        print("Rock you win")
+        user+=1
+    elif spr==3 and rps=="Paper":
+        print("Paper you win")
+        user+1
+    if spr==1 and rps=="Rock":
+        print("Rock tie")
+        tie+=1
+    elif spr== 2 and rps=="Paper":
+        print("Paper tie")
+        tie+=1
+    elif spr==3 and rps=="Scissors":
+        print("Scissors tie")
+        tie+=1
+    elif spr== "0":
+        done= True
+        print("User score:",user)
+        print("Computer score",com)
+        print("Ties:",tie)
+
+
+
+
+
+
+
 
 
 
--- 5.3_Camel_Game.py
+++ 5.3_Camel_Game.py
@@ -4,6 +4,63 @@ CAMEL GAME
 The pseudo-code for how to code this game is in Chapter 5 of the Python Jedi book
 
 '''
+miles=0
+energyshots=5
+energy=10
+cops=0
+gas=100
+print("You robbed a bank and the cops are following you.")
+print("The cars gas you stole is full.")
+print("YOu have 5 energy shots for the long road ahead.")
+print("Manage your energy and gas. Good luck")
+print()
+start=int(input("Click 0 to start"))
+print()
+if start==0:
+    done=False #When done=False the game starts when done=True the game will stop
+
+while not done:
+    print("A. Take an energy shot")
+    print("B. Ahead moderate speed")
+    print("C. Ahead Full speed")
+    print("D. Stop for the night")
+    print("E. Status check")
+    print("Q. quit")
+    user=input()
+    if user.upper()=="A":
+        energy+=2
+        energyshots-=1
+    elif user.upper()=="B":
+        miles+=40
+        gas-=25
+        energy-=2
+    elif user.upper()=="C":
+        miles+=70
+        gas-=50
+        energy-=4
+    elif user.upper()=="D":
+        gas=100
+        energy+=5
+    elif user.upper()=="E":
+        print("Miles traveled",miles)
+        print("Energy",energy)
+        print("Gas",gas)
+        print("The cops are this many miles behind you",miles-cops)
+    elif user.upper()=="Q":
+        done=True
+if done==True:
+    print("You lose better luck next time :(")
+
+
+
+
+
+
+
+
+
+
+
 
 
 
